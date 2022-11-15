21a9f720df098857a29d2ef7035c587940346eec
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2022-11-15 20:56:18 +0000

    generation des apk et fichier executable

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
-     /*
+      /*
       theme: ThemeData.dark().copyWith(
       scaffoldBackgroundColor: backgroundColor,
     ),
--- lib/screens/plus.dart
+++ lib/screens/plus.dart
@@ -1,10 +1,25 @@
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:tikodc/constants.dart';
 
-class UserPlusPage extends StatelessWidget {
+class UserPlusPage extends StatefulWidget {
   const UserPlusPage({Key? key}) : super(key: key);
 
   @override
+  State<UserPlusPage> createState() => _UserPlusPageState();
+}
+
+class _UserPlusPageState extends State<UserPlusPage> {
+       PageController _pageController = PageController(initialPage: 1, viewportFraction: 0.2);
+         int _selecteTab = 1;
+
+  @override
+    void dispose(){
+    super.dispose();
+    _pageController.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
      appBar: AppBar(),
@@ -18,14 +33,37 @@ class UserPlusPage extends StatelessWidget {
           Container(
             color: Colors.red,
             height: 90,
-            child: _buildCameraTemplateSelector(),
+            child: _buildCameraTemplateSelector(context),
           ),
         ],
       ),
     );
   }
-  Widget _buildCameraTemplateSelector(){
-   final List<String> postypes = ["Camera", "Quick", "Templates"];
-   return Stack( alignment: Alignment.topCenter,);
+
+  Widget _buildCameraTemplateSelector(context){
+    final List<String> posTypes = ["Camera", "Quick", "Templates"];
+   TextStyle style = Theme.of(context).textTheme.bodyText1!.copyWith(
+    fontSize: 13, color: Colors.white, fontWeight: FontWeight.bold);
+   return Stack( alignment: Alignment.topCenter,
+   children: [
+     Container(height: 45,
+       child: PageView.builder(
+         controller: _pageController,
+           itemCount: posTypes.length,
+           itemBuilder: (context, index){
+       return Container(
+         width: 50,
+         height: 50,
+         child: Text(
+           posTypes[index],
+           style: style,
+         ),
+       );
+     }
+     ),
+
+     ),
+   ],
+   );
   }
 }
